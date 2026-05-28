b857711ab1ca4a8bc3205fe9bd31ed326834ebef
yadavkapil23 <kydv901@gmail.com>
2026-05-28 07:45:00 +0530

    fastapi path , query , pydantic

---
+++ FASTAPI_3RD_YR/main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+class User(BaseModel):
+   id:int
+   age:int
+   salary:int
+   category:str
+   min_price:float
+   max_price:float
+   in_stock:bool
+
+app = FastAPI()
+
+users_db = []
+
+@app.get("/home")
+def get_home():
+    return "This is the New User"
+
+@app.get("/users/{user_id}")
+def get_user(user_id : int):
+    return {"ID of user is : ":user_id}
+
+@app.get("/users")
+async def get_filter(age:int=19, salary:int=50000):
+    return {
+        "Age is : " : age,
+        "Salary is : " : salary
+    }
+
+@app.get("/products")
+async def get_products(
+    category : Optional[str] = None,
+    min_price  : float = 0,
+    max_price : Optional[float] = None,
+    in_stock : bool = True
+):
+ return {
+    "The Category is : " : category,
+    "Minimum Price is : " : min_price,
+    "IS PRODUCT IN STOCK : " : in_stock,
+    "Maximum Retail Price : ": max_price
+}
+
+@app.post("/users")
+async def create_users(
+   name : str,
+   email : str,
+   contact : int,
+   address : str
+):
+   
+   users_db.append(users)
+   return {
+      "Message : " : "User Created Successfully",
+      "User" : {
+         "Name of user : " : name,
+         "Email of user : " : email,
+         "phone number : " : contact,
+         "nivaas sthaan : " : address
+      }
+   }
